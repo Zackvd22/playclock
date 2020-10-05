f725ed8467614e97c18b9dc78db4da5a968de0d4
Zackvd22 <42554592+Zackvd22@users.noreply.github.com>
2020-10-04 22:22:09 -0400

    Simple timer added to webpage

--- bareServer.js
+++ bareServer.js
@@ -10,14 +10,17 @@
 
 // ========== Imports ===============
 // import all requred node packages
-var   IO         = require('socket.io');  // include socket.io module for socket.io webpage
-const EXPRESS    = require('express')     // include express module for serving up webpages
-const HTTP       = require('http')        // include http module to set up express as an http server.
+var   IO        = require('socket.io');  // include socket.io module for socket.io webpage
+const EXPRESS   = require('express')     // include express module for serving up webpages
+const HTTP      = require('http')        // include http module to set up express as an http server.
+var Timer       = require('easytimer.js').Timer;
+
 
 const HTTP_PORT = 80;
-
 var expressApp = EXPRESS();
 var httpServer = HTTP.createServer(expressApp);
+
+var gameClock = new Timer({callback: gameClockUpdate,countdown : true});
 
 // use express to serve requests to the specified html pages
 expressApp.use(EXPRESS.static(__dirname + '/public/'));
@@ -39,18 +42,31 @@ httpServer.listen(HTTP_PORT, function(err){
 IO = IO(httpServer); // after creating an http.Server get the socket.io instance object. (had to do this to keep imports at the top)
 IO.listen(httpServer); // socket.io begins listening for http connections
 IO.on('connection', function (socket) {
-    console.log('User Connected');
-
+    gameClockUpdate();
     //socket.on('someFunction', someFunction(data));
-    socket.on('test', function(){netDiscovery()});
-
-    //Whenever someone disconnects this piece of code executed
-    socket.on('disconnect', function () {
-        console.log('User Disconnected');
-        IO.sockets.emit('connectionStatus', { description: "Disconnected" });
+    socket.on('gameClockStart', function(min, sec){
+        //gameClock.start({countdown: true, startValues: {'minutes': min,'seconds': sec}});   
+        gameClock.start(); 
+    });
+    socket.on('gameClockStop', function(){
+        gameClock.pause();
+    });
+    socket.on('gameClockReset', function(min, sec){
+        gameClock.stop();
+        gameClock.start({startValues: {'minutes': min,'seconds': sec}});
+        console.log(gameClock.getConfig());
+        gameClock.pause();
     });
 });
 
+gameClock.addEventListener('started', function (e) {
+    gameClockUpdate();
+});
+function gameClockUpdate(){
+    sec = gameClock.getTimeValues().seconds;
+    min = gameClock.getTimeValues().minutes;
+    IO.sockets.emit('gameClockUpdateTime', sec,min);
+}
 
 // handle exit gracefully
 process.on('SIGINT', () => {
--- public/frontEnd.js
+++ public/frontEnd.js
@@ -1,8 +1,29 @@
-//This handles the front end socket stuff
-
 var socket = io();
 
-
-function test(){
-    socket.emit('test');
+function gameClockStart(){
+    var min = document.getElementById("gameClockMin").value;
+    var sec = document.getElementById("gameClockSec").value;
+    console.log(min);
+    console.log(sec);
+    socket.emit('gameClockStart', parseInt(min), parseInt(sec));
 }
+function gameClockStop(){
+    socket.emit('gameClockStop');
+}
+function gameClockReset(){
+    var min = document.getElementById("gameClockMin").value;
+    var sec = document.getElementById("gameClockSec").value;
+    socket.emit('gameClockReset', parseInt(min), parseInt(sec));
+}
+function gameClockSet(sec, min){
+    socket.emit('gameClockReset', parseInt(min), parseInt(sec));
+}
+
+// write to user
+socket.on('gameClockUpdateTime', function(sec,min){
+    if(sec < 10){
+        sec = '0' + sec.toString();
+    }
+    var time = min.toString() + ':' + sec.toString();
+    document.getElementById("gameClockTime").innerHTML = time;
+});
